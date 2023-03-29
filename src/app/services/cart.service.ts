73c1466468ef5f14503ff4be5e8db725694df253
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-03-29 17:50:01 -0500

    get total function

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -24,6 +24,10 @@ export class CartService {
 
     this.cart.next({items});
     this._snackBar.open('1 item added to cart.', 'Ok', {duration: 3000})
+    console.log(this.cart.value);
+  }
 
+  getTotal(items: Array<CartItem>): number {
+    return items.map((item) => item.price * item.quantity).reduce((prev, current) => prev + current, 0)
   }
 }
