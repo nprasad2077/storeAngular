b4f2fe3ae594b6dc0779e2e4bfa069c78d8449fc
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-03-29 15:06:58 -0500

    cart table display

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -31,4 +31,8 @@ export class CartComponent implements OnInit {
     this.dataSource = this.cart.items;
   }
 
+  getTotal(items: Array<CartItem>): number {
+    return items.map((item) => item.price * item.quantity).reduce((prev, current) => prev + current, 0)
+  }
+
 }
