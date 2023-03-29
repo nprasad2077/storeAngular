66b8cd482794661832a76b7f5b17fc66211b00df
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-03-29 18:31:06 -0500

    clear single item

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -44,4 +44,8 @@ export class CartComponent implements OnInit {
     this.cartService.clearCart();
   }
 
+  onRemoveFromCart(item: CartItem): void {
+    this.cartService.removeFromCart(item);
+  }
+
 }
