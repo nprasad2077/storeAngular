bb453c04bb75c8bd704edb15c65a884ee2b64cba
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-03-30 13:06:48 -0500

    add to cart fix

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -13,7 +13,7 @@ export class CartService {
   addToCart(item: CartItem): void {
     const items = [...this.cart.value.items]
 
-    const itemInCart = items.find((_item) => _item.id === _item.id);
+    const itemInCart = items.find((_item) => _item.id === item.id);
 
     if (itemInCart) {
       itemInCart.quantity += 1;
